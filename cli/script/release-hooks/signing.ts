13a3933de2e29daf4dba67efc34e3ece57c01b63
ruslan-bikkinin <v-rubikk@microsoft.com>
2017-10-31 15:42:29 +0300

    Fix possible inclusion of signature to update when it shouldn't be

--- cli/script/release-hooks/signing.ts
+++ cli/script/release-hooks/signing.ts
@@ -19,6 +19,14 @@ interface CodeSigningClaims {
 
 var sign: cli.ReleaseHook = (currentCommand: cli.IReleaseCommand, originalCommand: cli.IReleaseCommand, sdk: AccountManager): q.Promise<cli.IReleaseCommand> => {
     if (!currentCommand.privateKeyPath) {
+        if (fs.lstatSync(currentCommand.package).isDirectory()) {
+            // If new update wasn't signed, but signature file for some reason still appears in the package directory - delete it
+            let signatureFilePath = path.join(currentCommand.package, METADATA_FILE_NAME);
+            if (fs.existsSync(signatureFilePath)) {
+                fs.unlinkSync(signatureFilePath);
+            }
+        }
+
         return q.resolve<cli.IReleaseCommand>(currentCommand);
     }
 
