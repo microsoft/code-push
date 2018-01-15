a69c8f82dfefccfa65fa0f374037a57f94d168ea
max-mironov <v-maximi@microsoft.com>
2018-01-15 11:53:45 +0300

    replace hotfix to codepush

--- sdk/test/acquisition-rest-mock.ts
+++ sdk/test/acquisition-rest-mock.ts
@@ -20,11 +20,11 @@ export var latestPackage = <rest.UpdateCheckResponse>{
 };
 
 export var serverUrl = "http://myurl.com";
-var hotFixBasePath = "/v0.1/public/hotfix";
-var serverUrlWithHotFixBasePath = serverUrl + hotFixBasePath;
-var reportStatusDeployUrl = serverUrlWithHotFixBasePath + "/report_status/deploy";
-var reportStatusDownloadUrl = serverUrlWithHotFixBasePath + "/report_status/download";
-var updateCheckUrl = serverUrlWithHotFixBasePath + "/update_check?";
+var codepushBasePath = "/v0.1/public/codepush";
+var serverUrlWithCodePushBasePath = serverUrl + codepushBasePath;
+var reportStatusDeployUrl = serverUrlWithCodePushBasePath + "/report_status/deploy";
+var reportStatusDownloadUrl = serverUrlWithCodePushBasePath + "/report_status/download";
+var updateCheckUrl = serverUrlWithCodePushBasePath + "/update_check?";
 
 export class HttpRequester implements acquisitionSdk.Http.Requester {
     public request(verb: acquisitionSdk.Http.Verb, url: string, requestBodyOrCallback: string | acquisitionSdk.Callback<acquisitionSdk.Http.Response>, callback?: acquisitionSdk.Callback<acquisitionSdk.Http.Response>): void {
