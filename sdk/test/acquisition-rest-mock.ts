edcb303fc1dda3e40c510a24d3cf55a60977ab7e
alexandergoncharov <alexander.goncharov@akvelon.com>
2019-08-19 12:02:00 +0300

    Refactore code

--- sdk/test/acquisition-rest-mock.ts
+++ sdk/test/acquisition-rest-mock.ts
@@ -20,9 +20,10 @@ export var latestPackage = <rest.UpdateCheckResponse>{
 };
 
 export var serverUrl = "http://myurl.com";
-var reportStatusDeployUrl = serverUrl + "/v0.1/public/codepush/report_status/deploy";
-var reportStatusDownloadUrl = serverUrl + "/v0.1/public/codepush/report_status/download";
-var updateCheckUrl = serverUrl + "/v0.1/public/codepush/update_check?";
+var publicPrefix = "/v0.1/public/codepush";
+var reportStatusDeployUrl = serverUrl + publicPrefix + "/report_status/deploy";
+var reportStatusDownloadUrl = serverUrl + publicPrefix + "/report_status/download";
+var updateCheckUrl = serverUrl + publicPrefix + "/update_check?";
 
 export class HttpRequester implements acquisitionSdk.Http.Requester {
     public request(verb: acquisitionSdk.Http.Verb, url: string, requestBodyOrCallback: string | acquisitionSdk.Callback<acquisitionSdk.Http.Response>, callback?: acquisitionSdk.Callback<acquisitionSdk.Http.Response>): void {
